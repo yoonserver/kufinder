1e91b1d4273482460760ac34bd1b83bdd0456908
yoonserver <yoonserver@gmail.com>
2022-03-04 00:02:07 +0900

    강의 정보 초기화 (과목코드, 과목명, 교수)

--- src/main/java/com/kufinder/service/LectureService.java
+++ src/main/java/com/kufinder/service/LectureService.java
@@ -1,11 +1,18 @@
 package com.kufinder.service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kufinder.domain.lecture.Lecture;
 import com.kufinder.domain.lecture.LectureRepository;
 import com.kufinder.web.dto.LectureListResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +22,24 @@ import lombok.RequiredArgsConstructor;
 public class LectureService {
 
     private final LectureRepository lectureRepository;
+
+    @PostConstruct
+    public void init() throws IOException {
+        String url = "https://kupis.konkuk.ac.kr/sugang/acd/cour/time/SeoulTimetableInfo.jsp?ltYy=2022&ltShtm=B01011&sbjtId=&pobtDiv=B04054";
+        Elements rows = Jsoup.connect(url).get().getElementsByTag("tbody").get(0).select("tr");
+
+        for (Element row : rows) {
+            Elements cells = row.getElementsByTag("td");
+            lectureRepository.save(Lecture.builder()
+                .id(Integer.valueOf(cells.get(3).text()))
+                .title(cells.get(4).text())
+                .professor(cells.get(9).text())
+                .currentCount(0)
+                .limitCount(0)
+                .build()
+            );
+        }
+    }
 
     @Transactional(readOnly = true)
     public List<LectureListResponseDto> findAll() {
